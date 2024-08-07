375af401c9a73c3a6a7b041b00876617299853f7
Phuc Lan Phan <160022282+PhucLan34@users.noreply.github.com>
2024-08-07 19:46:30 +1000

    Add SendEmail function

--- script.js
+++ script.js
@@ -8,3 +8,16 @@ var typed = new Typed(".text", {
 })
 
 
+function sendEmail() {
+    Email.send({
+        SecureToken : "C973D7AD-F097-4B95-91F4-40ABC5567812",
+        To : 'mitheo123@gmail.com',
+        From : document.getElementById("email").value,
+        Subject : document.getElementById("subject").value,
+        Body : "Name: " + document.getElementById("name").value 
+            + "<br> Email: " + document.getElementById("email").value
+            + "<br> Message: " + document.getElementById("message").value
+    }).then(
+      message => alert("Message Sent")
+    );
+}
