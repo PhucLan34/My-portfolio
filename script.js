af1920a5a4829d516dfe27191edf0c17e5587c93
Phuc Lan Phan <160022282+PhucLan34@users.noreply.github.com>
2024-08-09 03:09:47 +1000

    partial responsive

--- script.js
+++ script.js
@@ -8,16 +8,12 @@ var typed = new Typed(".text", {
 })
 
 
-function sendEmail() {
-    Email.send({
-        SecureToken: "e50d6e7e-9c99-4c84-8573-d3fb139add57",
-        To : 'mitthong12@gmail.com',
-        From : document.getElementById("email").value,
-        Subject : document.getElementById("subject").value,
-        Body : "Name: " + document.getElementById("name").value 
-            + "<br> Email: " + document.getElementById("email").value
-            + "<br> Message: " + document.getElementById("message").value
-    }).then(
-      message => alert("Message Sent")
-    );
+function showSideBar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+}
+
+function hideSideBar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
 }
